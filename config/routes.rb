932f3840d37aaefd75842f6dbaaf4f5ec8609b85
EHoldener <eholdener12@gmail.com>
2022-08-11 07:07:51 -0700

    started work on adding user authentication to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope 'api/v1' do
+    resources :users, only: %i[create show]
+  end
 
   # Defines the root path route ("/")
   root "pages#index"
