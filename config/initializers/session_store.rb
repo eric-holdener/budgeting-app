6acbf391d2f9262dd7dd8f510f35772308bfe53f
EHoldener <eholdener12@gmail.com>
2022-08-12 11:59:04 -0700

    wiped app to start over from ground up again

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_budgeting-app', domain: 'budgeting-app-json-api'
-else
-  Rails.application.config.session_store :cookie_store, key: '_budgeting-app'
-end
