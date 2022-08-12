4e97b2688cd5718e5f3c573a9b7e271356850def
EHoldener <eholdener12@gmail.com>
2022-08-12 13:46:38 -0700

    added a test account seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.first || User.create(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+cash_flows = [
+  {
+    title: 'Computer Chair',
+    description: 'Bought a chair from Herman Miller',
+    account: 'Debit',
+    amount: 1000,
+    date: Time.now
+  },
+  {
+    title: 'Paycheck',
+    description: 'Got paid for work',
+    account: 'Debit',
+    amount: 2000,
+    date: Time.now
+  }
+]
+
+cash_flows.each do |cf_hash|
+  user.cash_flows.create(cf_hash)
+end
